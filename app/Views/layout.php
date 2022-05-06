86b09b7a013d7b0615f4f22b7551edd612e88e36
Tim <burnedsyn@gmail.com>
2022-05-06 11:32:17 +0200

    ajout collection et début ajax

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
 	<meta charset="UTF-8">
 	<title>Welcome to CodeIgniter 4 Nft Art Generator!</title>
 	<meta name="description" content="complexe scene art generator">
@@ -192,7 +193,53 @@
 			}
 		}
 	</style>
-
+<script type="text/javascript">
+var es;
+ 
+ function startTask() {
+     es = new EventSource('/Sse');
+	 es.addEventListener("open", (event) => {
+  // Prints the information about an event
+  console.log(event);
+});
+     //a message is received
+     es.addEventListener('message', function(e) {
+         var result = JSON.parse( e.data );
+		 
+          
+		 addLog(result.message);       
+          
+         if(e.lastEventId == 'CLOSE') {
+			addLog('Received CLOSE closing');
+             es.close();
+             var pBar = document.getElementById('progressor');
+             pBar.value = pBar.max; //max out the progress bar
+         }
+         else {
+             var pBar = document.getElementById('progressor');
+             pBar.value = result.progress;
+             var perc = document.getElementById('percentage');
+             perc.innerHTML   = result.progress  + "%";
+             perc.style.width = (Math.floor(pBar.clientWidth * (result.progress/100)) + 15) + 'px';
+         }
+     });
+      
+     es.addEventListener('error', function(e) {
+		addLog("Error occurred : "+e.lastEventId);
+         es.close();
+     });
+ }
+  
+ function stopTask() {
+     es.close();
+     addLog('Interrupted');
+ }
+  
+ function addLog(message) {
+     var r = document.getElementById('results');
+     r.innerHTML += message + '<br>';
+     r.scrollTop = r.scrollHeight;
+ } </script>
 
 </head>
 <body>
@@ -210,7 +257,7 @@
 			<li class="menu-toggle">
 				<button onclick="toggleMenu();">&#9776;</button>
 			</li>
-			<li class="menu-item hidden"><a href="#">Home</a></li>
+			<li class="menu-item hidden"><a href="/">Home</a></li>
 			<li class="menu-item hidden"><a href="/collection">collection</a></li>
 			
 		</ul>
